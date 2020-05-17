166d95e79c24d3de1291f97eae4b33dc69408d46
Szabolcs Vasas <vasas@apache.org>
2020-05-17 18:22:01 +0100

    __to_string is properly implemented for date and timestamp columns.

--- dataframe_generator/outputter.py
+++ dataframe_generator/outputter.py
@@ -1,3 +1,4 @@
+import datetime
 from typing import Dict
 
 
@@ -8,9 +9,18 @@ def generate_csv(values: Dict, separator: str = ',', header: bool = True) -> str
         result += separator.join(values['field_names']) + new_line
     for row in values['values']:
         for column in row:
-            result += str(column)
+            result += __to_string(column)
             result += separator
         result = result[:-1]
         result += new_line
 
     return result
+
+
+def __to_string(column) -> str:
+    if isinstance(column, datetime.datetime):
+        return column.strftime("%Y-%m-%d %H:%M:%S")
+    elif isinstance(column, datetime.date):
+        return column.strftime("%Y-%m-%d")
+    else:
+        return str(column)
