be947172d62089252c2f1fa5527cdbd166159a05
Szabolcs Vasas <vasas@apache.org>
2020-05-17 17:20:18 +0100

    Value generators are implemented.

--- dataframe_generator/data_type.py
+++ dataframe_generator/data_type.py
@@ -1,4 +1,8 @@
 import re
+import string
+from datetime import date, timedelta, datetime
+from math import copysign
+from random import randint, choice
 
 
 class DataType:
@@ -18,17 +22,29 @@ class DataType:
 class ByteType(DataType):
     type_descriptor = r'ByteType\(\)'
 
+    def next_value(self) -> int:
+        return randint(-128, 127)
+
 
 class ShortType(DataType):
     type_descriptor = r'ShortType\(\)'
+
+    def next_value(self) -> int:
+        return randint(-32768, 32767)
 
 
 class IntegerType(DataType):
     type_descriptor = r'IntegerType\(\)'
 
+    def next_value(self) -> int:
+        return randint(-2147483648, 2147483647)
+
 
 class LongType(DataType):
     type_descriptor = r'LongType\(\)'
+
+    def next_value(self) -> int:
+        return randint(-9223372036854775808, 9223372036854775807)
 
 
 class DecimalType(DataType):
@@ -49,17 +65,37 @@ class DecimalType(DataType):
         self.scale = scale
         self.precision = precision
 
+    def next_value(self) -> int:
+        range_max = pow(10, self.scale) - 1
+        unscaled = randint(0, range_max)
+        signum = randint(-1, 0)
+        return copysign(unscaled, signum) / pow(10, self.precision)
+
 
 class StringType(DataType):
     type_descriptor = r'StringType\(\)'
+
+    def next_value(self, length=10) -> str:
+        letters = string.ascii_lowercase
+        return ''.join((choice(letters) for i in range(length)))
 
 
 class DateType(DataType):
     type_descriptor = r'DateType\(\)'
 
+    def next_value(self) -> date:
+        end = date.today()
+        random_days = randint(0, 1000)
+        return end - timedelta(days=random_days)
+
 
 class TimestampType(DataType):
     type_descriptor = r'TimestampType\(\)'
+
+    def next_value(self) -> datetime:
+        end = datetime.now()
+        random_seconds = randint(0, 100000000)
+        return end - timedelta(seconds=random_seconds)
 
 
 supported_types = [ByteType,
